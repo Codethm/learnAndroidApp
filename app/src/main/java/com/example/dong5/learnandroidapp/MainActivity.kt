1783c127e63e9d74eac63da6960a8627a1364008
Codethm <dong55.102375@gmail.com>
2018-08-01 01:26:16 +0700

    5Hr.++ Final

--- app/src/main/java/com/example/dong5/learnandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/dong5/learnandroidapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.dong5.learnandroidapp
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener({
+            link()
+        })
+    }
+
+    fun link(){
+
+        /*val url = "http://www.example.com"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)*/
+
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.android.com/training/constraint-layout"))
+        startActivity(i)
     }
 }
